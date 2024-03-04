c381a8c5613cf483e29e2ce62c1d7c60057800d2
hansvandenpol <hans.van.den.pol@validsign.nl>
2024-03-04 21:28:20 +0100

    updated tasks from items from database

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks',
+            product: [],
             image: './assets/images/socks_blue.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester']
         }
-    }
+    },
+    mounted () {
+        axios
+          .get('http://localhost:8080/task/all')
+          .then(response => {
+            var data = response.data;
+            console.log(data);
+            // data.forEach(i => {
+            //     if(!this.product.hasOwnProperty(i.category)) {
+            //         this.product[i.category] = [];
+            //         this.product[i.category].push(i);
+            //     } else {
+            //         this.product[i.category].push(i);
+            //     }
+            // });
+
+             data.forEach(i => {
+                var searchItem = this.product.find(x => x.category === i.category);
+                if(!searchItem){
+                    this.product.push({category: i.category, data: [i]})
+                } else {
+                    searchItem.data.push(i);
+                }
+            });
+            console.log(this.product);
+            
+        }
+          )
+      }
 })
