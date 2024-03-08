fdf36e59c022752a49e9ddc6590a385525272a04
hansvandenpol <hans.van.den.pol@validsign.nl>
2024-03-08 20:39:21 +0100

    created fail-safe when data cannot be loaded

--- main.js
+++ main.js
@@ -7,22 +7,10 @@ const app = Vue.createApp({
             details: ['50% cotton', '30% wool', '20% polyester']
         }
     },
-    mounted () {
-        axios
-          .get('http://localhost:8080/task/all')
-          .then(response => {
-            var data = response.data;
+    methods: {
+        processData(data) {
             console.log(data);
-            // data.forEach(i => {
-            //     if(!this.product.hasOwnProperty(i.category)) {
-            //         this.product[i.category] = [];
-            //         this.product[i.category].push(i);
-            //     } else {
-            //         this.product[i.category].push(i);
-            //     }
-            // });
-
-             data.forEach(i => {
+            data.forEach(i => {
                 var searchItem = this.product.find(x => x.category === i.category);
                 if(!searchItem){
                     this.product.push({category: i.category, data: [i]})
@@ -30,9 +18,25 @@ const app = Vue.createApp({
                     searchItem.data.push(i);
                 }
             });
-            console.log(this.product);
+        }
+      },
+    mounted () {
+        axios
+          .get('http://localhost:8080/task/all')
+          .then(response => {
+            var data = response.data;
+            localStorage.setItem("cachedData", JSON.stringify(data));
+
+            this.processData(data);
             
         }
-          )
+          ).catch(error => {
+            console.warn("Couldn't access server. Using cache from local storage");
+            var data = localStorage.getItem("cachedData");
+            //console.log("A: " + data);
+          //  console.log("B: " + JSON.parse(data)[0].name);
+            var parsed = JSON.parse(data);
+            this.processData(parsed);
+          });
       }
 })
