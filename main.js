0f3233ab095808851541d78bae75fd5dc188ffd8
hansvandenpol <hans.van.den.pol@validsign.nl>
2024-03-22 15:45:52 +0100

    added support for showing tasks for a specific month

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 const app = Vue.createApp({
     data() {
         return {
+            months: ["Januari","Februari","Maart","April","Mei","Juni","Juli","Augstus","September","Oktober","November","December"],
             product: [],
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            month: '',
+            previousElement: null
         }
     },
     methods: {
         processData(data) {
-            console.log(data);
+            this.product = [];
             data.forEach(i => {
                 var searchItem = this.product.find(x => x.category === i.category);
                 if(!searchItem){
@@ -18,25 +21,65 @@ const app = Vue.createApp({
                     searchItem.data.push(i);
                 }
             });
+        },
+        retrieveCurrentMonth() {
+            const currentDate = new Date();
+            this.month = this.months[currentDate.getMonth()];
+        },
+        showForMonth(month, event) {
+            this.month = this.months[month];
+            
+            if(event) {
+                const element = event.target;
+                console.log(this.previousElement)
+                console.log(element)
+                if(this.previousElement) {
+                    this.previousElement.classList.remove('active')
+                }
+                this.previousElement = element;
+
+                element.classList.add('active')
+            }
+
+            axios
+            .get('http://localhost:8080/task/month/' + month)
+            .then(response => {
+                var data = response.data;
+                this.processData(data);
+            }
+            ).catch(error => {
+                console.warn("Couldn't access server. Using cache from local storage");
+                this.product = null;
+            });
+
+        },
+        showForCurrent() {
+            axios
+            .get('http://localhost:8080/task/all')
+            .then(response => {
+                var data = response.data;
+                localStorage.setItem("cachedData", JSON.stringify(data));
+
+                this.processData(data);
+                
+            }
+            ).catch(error => {
+                console.warn("Couldn't access server. Using cache from local storage");
+                var data = localStorage.getItem("cachedData");
+
+                if(data) {
+                    var parsed = JSON.parse(data);
+                    this.processData(parsed);
+                }else {
+                    this.product = "Cannot load data and no cache available!"
+                }
+            });
         }
       },
     mounted () {
-        axios
-          .get('http://localhost:8080/task/all')
-          .then(response => {
-            var data = response.data;
-            localStorage.setItem("cachedData", JSON.stringify(data));
+        // Retrieve current month
+        this.retrieveCurrentMonth();
 
-            this.processData(data);
-            
-        }
-          ).catch(error => {
-            console.warn("Couldn't access server. Using cache from local storage");
-            var data = localStorage.getItem("cachedData");
-            //console.log("A: " + data);
-          //  console.log("B: " + JSON.parse(data)[0].name);
-            var parsed = JSON.parse(data);
-            this.processData(parsed);
-          });
+        this.showForCurrent();
       }
 })
