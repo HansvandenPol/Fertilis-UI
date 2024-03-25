9f2cc9b72ed23b8a9a5eb821a0b021a2ed1fb750
hansvandenpol <hans.van.den.pol@validsign.nl>
2024-03-25 14:04:32 +0100

    added more details in weather info

--- main.js
+++ main.js
@@ -11,6 +11,9 @@ const app = Vue.createApp({
             quickWeatherRainfall: '',
             quickWeatherWindDirection: '',
             quickWeatherAmbientMax: '',
+            quickWeatherWindSpeed: '',
+            cloudIndicatorIcon: 'fa-solid fa-sun',
+            cloudIndicatorColor: 'black'
         }
     },
     methods: {
@@ -45,7 +48,7 @@ const app = Vue.createApp({
             }
 
             axios
-            .get('http://localhost:8080/task/month/' + month)
+            .get('http://localhost:8081/task/month/' + month)
             .then(response => {
                 var data = response.data;
                 this.processData(data);
@@ -58,7 +61,7 @@ const app = Vue.createApp({
         },
         showForCurrent() {
             axios
-            .get('http://localhost:8080/task/all')
+            .get('http://localhost:8081/task/all')
             .then(response => {
                 var data = response.data;
                 localStorage.setItem("cachedData", JSON.stringify(data));
@@ -80,7 +83,7 @@ const app = Vue.createApp({
         },
         fetchQuickWeatherInfo() {
             axios
-            .get('http://localhost:8080/weather/knmi/quick')
+            .get('http://localhost:8081/weather/knmi/quick')
             .then(response => {
                 const data = response.data;
                 const statistics = data.statistics;
@@ -90,7 +93,16 @@ const app = Vue.createApp({
                         const statisticData = i.data;
                         statisticData.forEach(j => {
                             if(j.stationIndex == 32) {
-                                this.quickWeatherWindDirection = j.value;
+                                var direction = j.value;
+                                this.quickWeatherWindDirection = this.calculateDirectionText(direction);
+                            }
+                        });
+                    }
+                    else if(i.name === "stationname") {
+                        const statisticData = i.data;
+                        statisticData.forEach(j => {
+                            if(j.stationIndex == 32) {
+                                console.log('debug: station name ' + j.value);
                             }
                         });
                     }
@@ -98,7 +110,7 @@ const app = Vue.createApp({
                         const statisticData = i.data;
                         statisticData.forEach(j => {
                             if(j.stationIndex == 32) {
-                                this.quickWeatherRainfall = j.value;
+                                this.quickWeatherRainfall = Math.round(j.value * 10) / 10;
                             }
                         }); 
                     }
@@ -110,11 +122,105 @@ const app = Vue.createApp({
                             }
                         }); 
                     }
+                    else if(i.name === "Sav1H") {
+                        const statisticData = i.data;
+                        statisticData.forEach(j => {
+                            if(j.stationIndex == 32) {
+                                this.quickWeatherWindSpeed = this.setWindSpeedToBeaufort(j.value);
+                            }
+                        }); 
+                    }
+                    else if(i.name === "n") {
+                        const statisticData = i.data;
+                        statisticData.forEach(j => {
+                            if(j.stationIndex == 32) {
+                                this.setCloudIndicatorIcon(j.value)
+                            }
+                        }); 
+                    }
                 });
             }
             ).catch(error => {
                 console.warn("Couldn't access server.");
             });
+        },
+        calculateDirectionText(direction) {
+            console.log(direction);
+            var directionText;
+            if(direction <= 11.25 || direction >= 348.75 ) {
+                directionText = 'N';
+            } else if(direction >= 11.25 && direction < 33.75) {
+                directionText = 'NNO';
+            } else if(direction >= 33.75 && direction < 56.25) {
+                directionText = 'NO';
+            } else if(direction >= 56.25 && direction < 78.75) {
+                directionText = 'ONO';
+            } else if(direction >= 78.75 && direction < 101.25) {
+                directionText = 'O';
+            } else if(direction >= 101.25 && direction < 123.75) {
+                directionText = 'OZO';
+            } else if(direction >= 123.75 && direction <  146.25) {
+                directionText = 'ZO';
+            } else if(direction >= 146.25 && direction < 168.75) {
+                directionText = 'ZZO';
+            } else if(direction >= 168.75 && direction < 191.25) {
+                directionText = 'Z';
+            } else if(direction >= 191.25 && direction < 213.75) {
+                directionText = 'ZZW';
+            } else if(direction >= 213.75 && direction < 236.25) {
+                directionText = 'ZW';
+            } else if(direction >= 236.25 && direction < 258.75) {
+                directionText = 'WZW';
+            } 
+            else if(direction >= 258.75 && direction < 281.25) {
+                directionText = 'W';
+            } 
+            else if(direction >= 281.25 && direction < 303.75) {
+                directionText = 'WNW';
+            } 
+            else if(direction >= 303.75 && direction < 326.25) {
+                directionText = 'NW';
+            } 
+            else if(direction >= 326.25 && direction < 348.75) {
+                directionText = 'NNW';
+            } 
+            return directionText;
+        },
+        setCloudIndicatorIcon(cloudOctaValue) {
+            console.log('octa value: ' + cloudOctaValue);
+            
+            if(cloudOctaValue < 2 && cloudOctaValue >=0) {
+                this.cloudIndicatorIcon = "fa-solid fa-sun";
+                this.cloudIndicatorColor = "orange";
+            } else if(cloudOctaValue >= 2 && cloudOctaValue <= 4) {
+                this.cloudIndicatorIcon = "fa-solid fa-cloud-sun";
+                this.cloudIndicatorColor = "black";
+            } else if(cloudOctaValue == 5) {
+                this.cloudIndicatorIcon = "fa-solid fa-cloud";
+                this.cloudIndicatorColor = "lightgray";
+            } else if(cloudOctaValue == -9999.0) {
+                this.cloudIndicatorIcon = "fa-solid fa-cloud-sun";
+                this.cloudIndicatorColor = "black";
+            }
+            else {
+                this.cloudIndicatorIcon = "fa-solid fa-cloud";
+                this.cloudIndicatorColor = "gray"
+            }
+            console.log(this.cloudIndicatorColor);
+        }, setWindSpeedToBeaufort(speedMs) {
+            if(speedMs <= 0.2) {return 0;}
+            else if(speedMs > 0.2 && speedMs <= 1.5) {return 1;}
+            else if(speedMs > 1.5 && speedMs <= 3.3) {return 2;}
+            else if(speedMs > 3.3 && speedMs <= 5.4) {return 3;}
+            else if(speedMs > 5.4 && speedMs <= 7.9) {return 4;}
+            else if(speedMs > 7.9 && speedMs <= 10.7) {return 5;}
+            else if(speedMs > 10.7 && speedMs <= 13.8) {return 6;}
+            else if(speedMs > 13.8 && speedMs <= 17.1) {return 7;}
+            else if(speedMs > 17.1 && speedMs <= 20.7) {return 8;}
+            else if(speedMs > 20.7 && speedMs <= 24.4) {return 9;}
+            else if(speedMs > 28.4 && speedMs <= 28.4) {return 10;}
+            else if(speedMs > 32.6 && speedMs <= 32.6) {return 11;}
+            else {return 12;}
         }
       },
     mounted () {
@@ -124,5 +230,7 @@ const app = Vue.createApp({
         this.showForCurrent();
 
         this.fetchQuickWeatherInfo();
+
+        this.setCloudIndicatorIcon();
       }
 })
